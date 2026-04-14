76147d29ca7b651d19df6ad0941f2803ff34d2da
ArmanDesir <froschcrow@gmail.com>
2026-04-14 21:31:41 +0800

    t

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -7,7 +7,7 @@ class AddTaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add Task')),
-      body: const Center(child: Text('Add Task Screen')),
+      body: const Center(child: Text('Add Task Screen ')),
     );
   }
 }
